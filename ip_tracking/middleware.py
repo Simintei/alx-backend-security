2ab6612485fc218f45844b3ce1821b92f677c27c
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-07 15:04:37 +0300

    Create middleware.py

---
+++ ip_tracking/middleware.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponseForbidden
+from .models import BlockedIP
+
+class IPBlockMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip = self.get_client_ip(request)
+        if BlockedIP.objects.filter(ip_address=ip).exists():
+            return HttpResponseForbidden("Access denied: Your IP has been blocked.")
+        return self.get_response(request)
+
+    def get_client_ip(self, request):
+        """Retrieve client IP address safely."""
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
